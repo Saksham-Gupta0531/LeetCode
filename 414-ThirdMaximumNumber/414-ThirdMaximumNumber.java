00a67c5f5ffff8aee85a4e54169fa31934de6d36
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-07-31 12:29:37 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2025, 12:29:32 PM
+class Solution {
+    public int thirdMax(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int lastMax = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i = n - 1;i >= 0;i--){
+            if(nums[i] != lastMax){
+                count++;
+                lastMax = nums[i];
+            }
+            if(count == 3){
+                return nums[i];
+            }
+        }      
+        return nums[n - 1];
+    }
+}
