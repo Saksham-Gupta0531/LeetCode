f1e093df4a8a090056ccad701f11a2931a066110
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-07-31 12:29:46 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2025, 12:29:40 PM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magaHash = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            magaHash.put(c, magaHash.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magaHash.containsKey(c) || magaHash.get(c) <= 0) {
+                return false;
+            }
+            magaHash.put(c, magaHash.get(c) - 1);
+        }
+
+        return true;
+    }
+}
