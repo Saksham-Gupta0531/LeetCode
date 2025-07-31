6180c00f8bad41252fd2c68fbe8a1029297099ee
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-07-31 12:28:26 +0530

    Create 2059-UniqueLength3PalindromicSubsequences

---
+++ 2059-UniqueLength3PalindromicSubsequences/2059-UniqueLength3PalindromicSubsequences.java
@@ -0,0 +1,33 @@
+// Last updated: 7/31/2025, 12:28:21 PM
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+    int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (first[curr] == - 1) {
+                first[curr] = i;
+            }
+            
+            last[curr] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) {
+                continue;
+            }
+            
+            Set<Character> between = new HashSet();
+            for (int j = first[i] + 1; j < last[i]; j++) {
+                between.add(s.charAt(j));
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
+    }
+}
