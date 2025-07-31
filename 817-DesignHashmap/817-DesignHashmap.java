65726651e6bca8721dbb8fe3c6b233b73bac4d12
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-07-31 12:29:07 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.java
@@ -0,0 +1,17 @@
+// Last updated: 7/31/2025, 12:29:01 PM
+class MyHashMap {
+    int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
