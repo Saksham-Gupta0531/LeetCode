b986a48f3fd059bd8dcf1a51664ca0d46b7ceaf7
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-07-31 12:27:57 +0530

    Create 3788-MaximumUniqueSubarraySumAfterDeletion

---
+++ 3788-MaximumUniqueSubarraySumAfterDeletion/3788-MaximumUniqueSubarraySumAfterDeletion.py
@@ -0,0 +1,8 @@
+# Last updated: 7/31/2025, 12:27:51 PM
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        if all(n < 0 for n in nums):
+            return max(nums)
+        
+        unique = set(nums)
+        return sum(n for n in unique if n > 0)
