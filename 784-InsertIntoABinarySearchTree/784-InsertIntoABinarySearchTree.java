a3218cb85b84a88f8a9eb2183c0b549a1643bab5
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-09-03 17:28:15 +0530

    Create 784-InsertIntoABinarySearchTree

---
+++ 784-InsertIntoABinarySearchTree/784-InsertIntoABinarySearchTree.java
@@ -0,0 +1,29 @@
+// Last updated: 9/3/2025, 5:28:13 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root== null){
+            return new TreeNode(val);
+        }
+        if (root.val>val){
+            root.left=insertIntoBST(root.left,val);
+        }
+        else {
+            root.right=insertIntoBST(root.right,val);
+        }
+        return root;
+}}
