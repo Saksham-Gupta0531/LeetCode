9ff5ca441735c1b5bfa4a6a499d4421454d487a5
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-09-04 06:15:09 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.py
@@ -0,0 +1,7 @@
+# Last updated: 9/4/2025, 6:15:07 AM
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if target in nums:
+            return True
+        else :
+            return False
