1f9b61ce81501b64508b8997425758f585a5271c
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-09-05 08:17:36 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,27 @@
+// Last updated: 9/5/2025, 8:17:33 AM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[amount+1][coins.length];
+        for (int[] a: dp){
+            Arrays.fill(a,-1);
+        }
+        return Coin_charge(coins , amount , 0, dp);
+    }
+     public int Coin_charge(int[] coin ,int amount,int i ,int[][] dp){
+        if (amount == 0){
+            return 1;
+        }
+        if (i== coin.length){
+            return 0;
+        }
+        if (dp[amount][i] != -1){
+            return dp[amount][i];
+        }
+        int inc =0 ,exc =0;
+        if (amount >= coin[i]){
+            inc = Coin_charge(coin , amount -coin[i] ,i,dp);
+        }
+        exc = Coin_charge(coin ,amount ,i+1 ,dp);
+        return dp[amount][i] =inc +exc ;
+    }
+}
