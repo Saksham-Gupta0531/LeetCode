70f917262e60da79dca28779ffb2ff4c12334b4a
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-09-16 19:54:29 +0530

    Create 2188-MinimumTimetoFinishtheRace

---
+++ 2188-MinimumTimetoFinishtheRace/version/java/2188-MinimumTimetoFinishtheRace_20250916_195426.java
@@ -0,0 +1,20 @@
+// Last updated: 9/16/2025, 7:54:26 PM
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        HashMap<String ,Integer>h1= new HashMap<>();
+        HashMap<String ,Integer>h2= new HashMap<>();
+        for (String w : words1) {
+            h1.put(w, h1.getOrDefault(w, 0) + 1);
+        }
+        for (String w : words2) {
+            h2.put(w, h2.getOrDefault(w, 0) + 1);
+        }
+        int c=0;
+        for (String s : h1.keySet()) {
+            if (h1.get(s) == 1 && h2.containsKey(s) && h2.get(s) == 1) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
