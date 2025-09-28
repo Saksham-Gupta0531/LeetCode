285dd580c36d86e6ccf96258affe462e667cee9a
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-09-28 22:20:23 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/version/java/506-RelativeRanks_20250928_222019.java
@@ -0,0 +1,26 @@
+// Last updated: 9/28/2025, 10:20:19 PM
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for(int  i= 0 ; i< score.length ;i++){
+            q.add(new int[]{score[i],i});
+        }
+    
+        String[] s= new String[q.size()];
+        int rank = 1;
+        while (!q.isEmpty()) {
+            int[] a = q.poll();
+            if (rank ==1) {
+                s[a[1]] ="Gold Medal";
+            } else if (rank ==2) {
+                s[a[1]] ="Silver Medal";
+            } else if (rank ==3) {
+                s[a[1]] ="Bronze Medal";
+            } else {
+                s[a[1]] =Integer.toString(rank);
+            }
+            rank++;
+        }
+        return s;
+    }
+}
