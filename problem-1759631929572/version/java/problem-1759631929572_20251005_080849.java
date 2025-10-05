1a704de74fb056b119335b42e43cab69e5101bbb
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-05 08:08:54 +0530

    Create

---
+++ problem-1759631929572/version/java/problem-1759631929572_20251005_080849.java
@@ -0,0 +1,15 @@
+// Last updated: 10/5/2025, 8:08:49 AM
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0;
+        for(int i= 0 ; i< nums.length ;i++){
+            if (i%2==0){
+                sum+=nums[i];
+            }
+            else{
+                sum-=nums[i];
+            }
+        }
+        return sum;
+    }
+}
