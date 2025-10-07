cc18638a84f7ba06c4a46b55a3416b361ccb7c01
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-07 09:20:09 +0530

    Create 209-MinimumSizeSubarraySum

---
+++ 209-MinimumSizeSubarraySum/version/java/209-MinimumSizeSubarraySum_20251007_092009.java
@@ -0,0 +1,14 @@
+// Last updated: 10/7/2025, 9:20:09 AM
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, s = 0, m = Integer.MAX_VALUE;
+        for (int r = 0; r < nums.length; r++) {
+            s += nums[r];
+            while (s >= target) {
+                m = Math.min(m, r - l + 1);
+                s -= nums[l++];
+            }
+        }
+        return (m == Integer.MAX_VALUE) ? 0 : m;
+    }
+}
