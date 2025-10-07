e4b77177b9737960bdf49eec5ec4413d9c41f7b4
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-07 08:49:48 +0530

    Create 1544-MakeTheStringGreat

---
+++ 1544-MakeTheStringGreat/version/java/1544-MakeTheStringGreat_20251007_084948.java
@@ -0,0 +1,21 @@
+// Last updated: 10/7/2025, 8:49:48 AM
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+        
+        return result.toString();
+    }
+}
