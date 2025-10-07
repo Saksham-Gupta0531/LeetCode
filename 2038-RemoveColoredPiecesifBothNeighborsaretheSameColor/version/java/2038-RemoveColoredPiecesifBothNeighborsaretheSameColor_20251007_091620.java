28699e6e4a508230b9b9e97ad96f798c27da96b6
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-07 09:16:20 +0530

    Create 2038-RemoveColoredPiecesifBothNeighborsaretheSameColor

---
+++ 2038-RemoveColoredPiecesifBothNeighborsaretheSameColor/version/java/2038-RemoveColoredPiecesifBothNeighborsaretheSameColor_20251007_091620.java
@@ -0,0 +1,17 @@
+// Last updated: 10/7/2025, 9:16:20 AM
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int countA = 0;
+        int countB = 0;
+
+        for (int i = 1; i < colors.length() - 1; i++) {
+            if (colors.charAt(i-1) == 'A' && colors.charAt(i) == 'A' && colors.charAt(i+1) == 'A') {
+                countA ++;
+            }
+            if (colors.charAt(i-1) == 'B' && colors.charAt(i) == 'B' && colors.charAt(i+1) == 'B') {
+                countB ++;
+            }
+        }
+        return countA > countB;
+    }
+}
