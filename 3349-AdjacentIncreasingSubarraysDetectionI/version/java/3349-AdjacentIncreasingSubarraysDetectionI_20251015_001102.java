bbcf9cfe694183ba6aa7c8e72195128b89a3f2e0
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-15 00:11:05 +0530

    Create 3349-AdjacentIncreasingSubarraysDetectionI

---
+++ 3349-AdjacentIncreasingSubarraysDetectionI/version/java/3349-AdjacentIncreasingSubarraysDetectionI_20251015_001102.java
@@ -0,0 +1,16 @@
+// Last updated: 10/15/2025, 12:11:02 AM
+class Solution {
+  public boolean hasIncreasingSubarrays(List<Integer> A, int k) {
+        int n = A.size(), up = 1, pre_max_up = 0, res = 0;
+        for (int i = 1; i < n; ++i) {
+            if (A.get(i) > A.get(i - 1)) {
+                up++;
+            } else {
+                pre_max_up = up;
+                up = 1;
+            }
+            res = Math.max(res, Math.max(up / 2, Math.min(pre_max_up, up)));
+        }
+        return res >= k;
+    }
+}
