4ab81a2f54f42e83c999bf4d1b4097f69b162cc4
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-21 22:37:43 +0530

    Create 402-RemoveKDigits

---
+++ 402-RemoveKDigits/version/java/402-RemoveKDigits_20251021_223740.java
@@ -0,0 +1,32 @@
+// Last updated: 10/21/2025, 10:37:40 PM
+import java.util.*;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peekLast() > digit) {
+                stack.pollLast();
+                k--;
+            }
+            stack.addLast(digit);
+        }
+
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pollLast();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            char ch = stack.pollFirst();
+            if (sb.length() == 0 && ch == '0') continue;
+            sb.append(ch);
+        }
+
+        if(sb.isEmpty()) 
+            sb.append('0'); 
+        return sb.toString();
+    }
+}
