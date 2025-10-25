f14d0308c939afce33feba43b048936a8ce3c638
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-10-25 23:18:29 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/version/java/37-SudokuSolver_20251025_231828.java
@@ -0,0 +1,40 @@
+// Last updated: 10/25/2025, 11:18:28 PM
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solve(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int r, int c, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[r][i] == ch) return false;
+            if (board[i][c] == ch) return false;
+        }
+
+        int startRow = (r / 3) * 3;
+        int startCol = (c / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == ch) return false;
+            }
+        }
+        return true;
+    }
+}
