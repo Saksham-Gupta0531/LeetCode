861272ce74ead34e9a73023df70fc7635bd1f795
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2025-11-09 08:40:39 +0530

    Create

---
+++ problem-1762657836095/version/java/problem-1762657836095_20251109_084036.java
@@ -0,0 +1,28 @@
+// Last updated: 11/9/2025, 8:40:36 AM
+class Solution {
+    public int minimumDistance(int[] nums) {
+    HashMap<Integer,ArrayList<Integer>> map= new HashMap<>();
+        for (int i=0 ; i< nums.length ; i++){
+            if (!map.containsKey(nums[i]))
+                map.put(nums[i] ,new ArrayList<>());
+            map.get(nums[i]).add(i);
+        }
+        int sum= Integer.MAX_VALUE;
+        for ( ArrayList<Integer> values : map.values() ){
+            
+            if (values.size() >= 3){
+                int t=0;
+                for (int p = 0 ; p<=values.size()-3 ;p++){
+                    int i =values.get(p);
+                    int j = values.get(p+1);
+                    int k= values.get(p+2);
+                    t = Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i);
+                    if(t<sum ) sum =t;
+                }
+            }
+        }
+        if (sum == Integer.MAX_VALUE) return -1;
+        return sum ;
+        
+    }
+}
