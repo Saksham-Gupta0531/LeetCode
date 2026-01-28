f572fe379b8946e1be8494e2d8dc34f77b389df7
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-01-28 23:45:37 +0530

    Create 2250-CountNumberofRectanglesContainingEachPoint

---
+++ 2250-CountNumberofRectanglesContainingEachPoint/version/java/2250-CountNumberofRectanglesContainingEachPoint_20260128_234533.java
@@ -0,0 +1,36 @@
+// Last updated: 1/28/2026, 11:45:33 PM
+1class Solution {
+2    public int[] countRectangles(int[][] rectangles, int[][] points) {
+3        List<Integer>[] arr = new ArrayList[101];
+4        for (int i = 0; i < arr.length; i++)
+5            arr[i] = new ArrayList<Integer>();
+6        for (int[] rec : rectangles)
+7            arr[rec[1]].add(rec[0]);
+8        for (int i = arr.length - 1; i >= 0; i--) {
+9            if (i != arr.length - 1)
+10                arr[i].addAll(arr[i + 1]);
+11            Collections.sort(arr[i]);
+12        }
+13        // System.out.println(Arrays.deepToString(arr));
+14        int[] res = new int[points.length];
+15        for (int i = 0; i < points.length; i++) {
+16            int idx = bSearch(arr[points[i][1]], points[i][0]);
+17            res[i] = arr[points[i][1]].size() - idx;
+18            
+19        //System.out.println(Arrays.toString(points[i]) + "|" + arr[points[i][1]] + "|" + idx + "|" + res[i] + "%");
+20        }
+21        return res;
+22    }
+23
+24    int bSearch(List<Integer> list, int n) {
+25        int l = 0, r = list.size();
+26        while (l < r) {
+27            int mid = l + (r - l) / 2;
+28            if (n > list.get(mid))
+29                l = mid + 1;
+30            else
+31                r = mid;
+32        }
+33        return l;
+34    }
+35}
