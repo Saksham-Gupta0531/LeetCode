f399c51f481ebd2849a483c8dd391e69dfe80a17
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:28:11 +0530

    Create 2374-StepsToMakeArrayNonDecreasing

---
+++ 2374-StepsToMakeArrayNonDecreasing/2374-StepsToMakeArrayNonDecreasing.java
@@ -0,0 +1,16 @@
+// Last updated: 2/4/2026, 12:28:11 PM
+class Solution {
+    public int totalSteps(int[] nums) {
+        LinkedList<int[]> stack = new LinkedList<>();
+        int max = 0;
+        for(int num : nums) {
+            int score = 0;
+            while(stack.size() > 0 && stack.getLast()[0] <= num) {
+                score = Math.max(score, stack.removeLast()[1]);
+            }
+            stack.add(new int[]{num, stack.size() == 0 ? 0 : score+1});
+            max = Math.max(max, stack.getLast()[1]);
+        }
+        return max;
+    }
+}
