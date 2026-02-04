4a593a9f07ba44fcfbb16d7da9ea76c51555c9c1
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:29:02 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.java
@@ -0,0 +1,6 @@
+// Last updated: 2/4/2026, 12:29:02 PM
+class Solution {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
