635b58a1830d7ef4205653650348919ff956dfec
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:28:49 +0530

    Create 2049-EliminateMaximumNumberOfMonsters

---
+++ 2049-EliminateMaximumNumberOfMonsters/2049-EliminateMaximumNumberOfMonsters.java
@@ -0,0 +1,25 @@
+// Last updated: 2/4/2026, 12:28:49 PM
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        if (dist.length == 0 || speed.length == 0) {
+            return 0;
+        }
+
+        double[] time = new double[dist.length];
+        for (int i = 0; i < dist.length; i++) {
+            time[i] = (double) dist[i] / speed[i];
+        }
+
+        Arrays.sort(time);
+
+        int count = 0;
+        for (int i = 0; i < time.length; i++) {
+            if (time[i] <= i) {
+                break;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
