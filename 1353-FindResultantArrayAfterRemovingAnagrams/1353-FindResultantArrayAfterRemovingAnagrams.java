521884fb1bf0f1d160102c57b75a6f9f58fa93d8
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:29:50 +0530

    Create 1353-FindResultantArrayAfterRemovingAnagrams

---
+++ 1353-FindResultantArrayAfterRemovingAnagrams/1353-FindResultantArrayAfterRemovingAnagrams.java
@@ -0,0 +1,17 @@
+// Last updated: 2/4/2026, 12:29:50 PM
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        String prev ="";
+        List<String> li=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            char[] ch=words[i].toCharArray();
+            Arrays.sort(ch);
+            String curr=String.valueOf(ch);
+            if(!curr.equals(prev)){
+                li.add(words[i]);
+                prev=curr;
+            }
+        }
+        return li;
+    }
+}
