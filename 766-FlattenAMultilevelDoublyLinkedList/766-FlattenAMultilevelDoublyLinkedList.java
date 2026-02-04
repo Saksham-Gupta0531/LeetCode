f24e56a1a2f979e464584e71812aee911932aff3
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:30:51 +0530

    Create 766-FlattenAMultilevelDoublyLinkedList

---
+++ 766-FlattenAMultilevelDoublyLinkedList/766-FlattenAMultilevelDoublyLinkedList.java
@@ -0,0 +1,37 @@
+// Last updated: 2/4/2026, 12:30:51 PM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Node temp =head ;
+        Stack<Node> st = new Stack<>();
+        while(temp!=null && (temp.next !=null || temp.child != null) ){
+            if ( temp.child != null ){
+                if (temp.next!=null)
+                    st.push(temp.next);
+                temp.next= temp.child;
+                temp.next.prev = temp; 
+                temp.child = null ;
+            }
+            temp = temp.next;
+        }   
+        while(!st.isEmpty()){
+            temp.next = st.pop();
+            temp.next.prev = temp;
+            temp.child = null ;
+            while(temp.next!=null){
+                
+                temp= temp.next;
+            }
+        }
+        return head;
+    }
+}
