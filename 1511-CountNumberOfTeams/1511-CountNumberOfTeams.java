aac3d8bfeeef3f2ac6ed068805ad7af1376ab059
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:29:37 +0530

    Create 1511-CountNumberOfTeams

---
+++ 1511-CountNumberOfTeams/1511-CountNumberOfTeams.java
@@ -0,0 +1,16 @@
+// Last updated: 2/4/2026, 12:29:36 PM
+class Solution {
+    public int numTeams(int[] rating) {
+        int n =rating.length;
+        int c =0;
+        for (int i = 0;i <n-2 ;i++){
+            for( int j = i+1; j< n-1 ; j++){
+                for (int  k = j+1 ; k< n ;k++){
+                    if ((rating[i] < rating[j] && rating[j]< rating[k]) || (rating[i] > rating[j] && rating[j]> rating[k]))
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
