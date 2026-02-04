8467b80f719b64eeb956ede4d5c62e990952c4c4
Saksham Gupta <142567386+Saksham-Gupta0531@users.noreply.github.com>
2026-02-04 12:32:13 +0530

    Create 394-DecodeString

---
+++ 394-DecodeString/394-DecodeString.java
@@ -0,0 +1,34 @@
+// Last updated: 2/4/2026, 12:32:12 PM
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch != ']'){
+                stk.push(ch);
+            }
+            else{
+                StringBuilder sub = new StringBuilder();
+                while(stk.peek() != '['){
+                    sub.insert(0, stk.pop());
+                }
+                stk.pop();
+
+                StringBuilder num = new StringBuilder();
+                while(!stk.isEmpty() && Character.isDigit(stk.peek())){
+                    num.insert(0, stk.pop());
+                }
+                int k = Integer.parseInt(num.toString());
+
+                String rep = sub.toString().repeat(k);
+                for(char c : rep.toCharArray()){
+                    stk.push(c);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : stk){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
